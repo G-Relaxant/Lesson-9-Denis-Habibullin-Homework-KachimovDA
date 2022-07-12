e9ce09662c8dafbf8b5418765bf0075aad3e1c33
geekbrains <info@geekbrains.ru>
2022-07-12 14:20:53 +0400

    complete

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,25 +2,32 @@
 double m = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input number N: ");
 double n = Convert.ToDouble(Console.ReadLine());
-//int ak = 0;
+double ak = 0;
 double akkerman = AkkermanFunction(m, n);
-Console.WriteLine(akkerman);
+Console.WriteLine($"Answer: {akkerman}");
 
 double AkkermanFunction(double a, double b)
 {
     if(a == 0)
     {
-       return n + 1;
+       return ak = b + 1;
     }
     else
     {
         if(a > 0 && b == 0)
         {
-            return AkkermanFunction(a - 1, 1);
+            return ak =  AkkermanFunction(a - 1, 1);
         }
         else
         {
-            return AkkermanFunction(a - 1, AkkermanFunction(a, b - 1));
+            if(a > 0 && b > 0)
+            {
+                return ak = AkkermanFunction(a - 1, AkkermanFunction(a, b - 1));
+            }
+            else
+            {
+                return ak;
+            }
         }
     }
 }
